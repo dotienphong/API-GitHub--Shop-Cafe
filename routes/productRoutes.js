d18852c9cd29e52364926dd34415c59bfeb61475
PhongDev <dotienphong1993@gmail.com>
2022-06-15 16:39:32 +0700

    ok

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -38,14 +38,9 @@ app.delete("/coffeeShop/:id", async (req, res) => {
 //UPDATE (sửa)
 app.patch("/coffeeShop/:id", async (req, res) => {
     try {
-        const product = await productModel.findByIdAndUpdate(req.params.id,
-            {
-                price: req.data,
-            }
-        )
+        const product = await productModel.findByIdAndUpdate(req.params.id, req.body)
         await productModel.save()
-        res.send(product)
-
+        res.status(200).send(product)
     } catch (err) {
         res.status(500).send(err)
     }
